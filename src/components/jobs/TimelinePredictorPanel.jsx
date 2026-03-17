076c2ab91b8d27a815d7c0aa6fb7113d4efba4ed
Michael Huwe <drmhuwe@gmail.com>
2026-03-17 18:38:04 +0000

    File changes

--- src/components/jobs/TimelinePredictorPanel.jsx
+++ src/components/jobs/TimelinePredictorPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
